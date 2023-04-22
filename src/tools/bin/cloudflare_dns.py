9cbfdbe9be4d57120026d5208226c08947eba1f1
Nicola Canepa <canne74@gmail.com>
2023-04-22 11:01:58 +0100

    [cloudflare_dns.py] Implement quiet/verbose, fix single record
    * support `--verbose`/`--quiet`
    * single record update (without `-A`) was broken
    * fix unit tests

--- src/tools/bin/cloudflare_dns.py
+++ src/tools/bin/cloudflare_dns.py
@@ -17,17 +17,34 @@ MY_ZONES = {
 }
 
 
+class Echo(object):
+    def __init__(self, verbose: bool, quiet: bool):
+        self.verbose = verbose
+        self.quiet = quiet
+
+    def info(self, *args, **kwargs):
+        if not self.quiet:
+            return click.echo(*args, **kwargs)
+
+    def debug(self, *args, **kwargs):
+        if self.verbose:
+            return click.echo(*args, **kwargs)
+
+
 def show_records(cf_dict: dict):
     zones = defaultdict(list)
     for record in cf_dict["result"]:
-        zones[record["zone_id"]].append({k: record[k] for k in ("id", "name", "type", "content", "comment") if record[k]})
+        zones[record["zone_id"]].append(
+            {k: record[k] for k in ("id", "name", "type", "content", "comment") if record.get(k, None)}
+        )
     click.echo(json.dumps(zones, indent=2))
 
 
 @click.group()
 @click.option("--zone", default="nicolacanepa.net")
+@click.option("--verbose/--quiet", "-v/-q", default=None)
 @click.pass_context
-def main(ctx, zone: str):
+def main(ctx, zone: str, verbose: bool):
     ctx.ensure_object(dict)
     with open(os.path.join(os.environ["HOME"], ".cloudflare.cfg"), "r") as f:
         MY_ZONES.update(json.load(f))
@@ -35,7 +52,10 @@ def main(ctx, zone: str):
     zone_id = MY_ZONES.get(zone, {}).get("id", None)
     ctx.obj["base_url"] = f"https://api.cloudflare.com/client/v4/zones/{zone_id}/dns_records"
     auth_token = MY_ZONES.get(zone, {}).get("token", None)
-    ctx.obj["headers"] ={"Content-Type": "application/json", "Authorization": f"Bearer {auth_token}"}
+    ctx.obj["headers"] = {"Content-Type": "application/json", "Authorization": f"Bearer {auth_token}"}
+    quiet = verbose is False
+    verbose = verbose or False
+    ctx.obj["echo"] = Echo(verbose=verbose, quiet=quiet)
 
 
 @main.command("list")
@@ -49,7 +69,7 @@ def records(ctx):
         show_records(response_json)
     else:
         for message in ("errors", "messages"):
-            click.echo(response_json[message])
+            ctx.obj["echo"].info(response_json[message])
 
 
 @main.command()
@@ -61,34 +81,47 @@ def records(ctx):
 def update(ctx, ip_address: str, fqdns: typing.Iterable[str], all_records: bool, unsafe: bool):
     headers = ctx.obj["headers"]
     my_zone = MY_ZONES.get(ctx.obj["zone"], {})
+    echo = ctx.obj["echo"]
+
     if all_records:
         url = ctx.obj['base_url']
         response = requests.get(url, headers=headers).json()
         zones = defaultdict(list)
         for record in response["result"]:
             if record["comment"] == "Donomore":
+                echo.debug(f"Adding {record['name']}")
                 local_record = record.copy()
                 my_zone[local_record["name"]] = local_record
                 fqdns = fqdns + (local_record["name"],)
+    else:
+        fqdns = fqdns or (f"www.{ctx.obj['zone']}",)
+        for fqdn in fqdns:
+            echo.debug(f"Checking {fqdn}")
+            record_id = my_zone[fqdn]["id"]
+            url = f"{ctx.obj['base_url']}/{record_id}"
+            response = requests.get(url, headers=headers).json()
+            my_zone[fqdn] = response["result"][0]
 
-    for fqdn in fqdns or (f"www.{ctx.obj['zone']}",):
+    for fqdn in fqdns:
+        click.echo(fqdn)
         record_data = my_zone[fqdn]
+        click.echo(record_data)
         record_id = record_data["id"]
         url = f"{ctx.obj['base_url']}/{record_id}"
-        if ip_address == record_data["content"]:
-            click.echo(f"{fqdn} already points to {ip_address}")
+        if ip_address == record_data.get("content", None):
+            echo.info(f"{fqdn} already points to {ip_address}")
             continue
         record_data["content"] = ip_address
         if unsafe:
             response = requests.put(url, headers=headers, json=record_data)
             response_json = response.json()
             if response_json["success"]:
-                click.echo(response_json)
+                echo.info(response_json)
             else:
                 for message in ("errors", "messages"):
-                    click.echo(response_json[message])
+                    echo.info(response_json[message])
         else:
-            click.echo(f"Would PUT: {url}, with headers=(hidden), json={record_data}")
+            echo.info(f"Would PUT: {url}, with headers=(hidden), json={record_data}")
 
 
 if __name__ == "__main__":
--- test/test_cloudflare_dns.py
+++ test/test_cloudflare_dns.py
@@ -9,7 +9,17 @@ from tools.bin.cloudflare_dns import main
 def mock_requests(monkeypatch):
     mock_obj = mock.MagicMock(name='requests')
     mock_obj.get.return_value.json.return_value = {
-        'result': [{"zone_id": "ZONEID", "id": "ID", "name": "www.mydomain", "type": "A", "content": "10.11.12.13"}],
+        'result': [
+            {
+                "zone_id": "XXX",
+                "id": "IDAUTH",
+                "name": "www.mydomain",
+                "type": "A",
+                'proxied': True,
+                'ttl': 1,
+                "content": "10.11.12.33",
+            }
+        ],
         'success': True,
     }
     monkeypatch.setattr('tools.bin.cloudflare_dns.requests', mock_obj)
@@ -33,7 +43,15 @@ def mock_my_zones(monkeypatch):
 
 @pytest.fixture(autouse=True)
 def mock_open(monkeypatch):
-    mock_obj = mock.mock_open(read_data='{}')
+    domain = "nicolacanepa.net"
+    mock_cfg = f'''{{
+        "{domain}": {{
+            "id": "NNN",
+            "token": "CCC",
+            "www.{domain}": {{"id": "UUU", "type": "A", "name": "www.{domain}", "proxied": true, "ttl": 1}}
+        }}
+    }}'''
+    mock_obj = mock.mock_open(read_data=mock_cfg)
     monkeypatch.setattr('builtins.open', mock_obj)
     yield mock_obj
     print(f'{mock_obj}: {mock_obj.mock_calls}')
@@ -49,6 +67,16 @@ def test_list(mock_requests, mock_my_zones):
     )
 
 
+def test_list_default(mock_requests, mock_my_zones):
+    runner = CliRunner()
+    result = runner.invoke(main, ['list'])
+    assert result.exit_code == 0
+    mock_requests.get.assert_called_with(
+        'https://api.cloudflare.com/client/v4/zones/NNN/dns_records',
+        headers={'Content-Type': 'application/json', 'Authorization': 'Bearer CCC'},
+    )
+
+
 def test_list_unknown_domain(mock_requests, mock_my_zones):
     runner = CliRunner()
     result = runner.invoke(main, ['--zone', 'otherdomain', 'list'])
@@ -61,7 +89,7 @@ def test_list_unknown_domain(mock_requests, mock_my_zones):
 
 def test_update(mock_requests, mock_my_zones):
     runner = CliRunner()
-    result = runner.invoke(main, ['--zone', 'mydomain', 'update', '10.11.12.13'])
+    result = runner.invoke(main, ['-v', '--zone', 'mydomain', 'update', '10.11.12.13'])
     assert result.exit_code == 0
     mock_requests.put.assert_not_called()
 
@@ -71,7 +99,15 @@ def test_update_unsafe(mock_requests, mock_my_zones):
     result = runner.invoke(main, ['--zone', 'mydomain', 'update', '--unsafe', '10.11.12.13'])
     assert result.exit_code == 0
     mock_requests.put.assert_called_with(
-        'https://api.cloudflare.com/client/v4/zones/XXX/dns_records/ZZZ',
+        'https://api.cloudflare.com/client/v4/zones/XXX/dns_records/IDAUTH',
         headers={'Content-Type': 'application/json', 'Authorization': 'Bearer YYY'},
-        json={'id': 'ZZZ', 'type': 'A', 'name': 'www.mydomain', 'proxied': True, 'ttl': 1, 'content': '10.11.12.13'},
+        json={
+            'zone_id': 'XXX',
+            'id': 'IDAUTH',
+            'type': 'A',
+            'name': 'www.mydomain',
+            'proxied': True,
+            'ttl': 1,
+            'content': '10.11.12.13',
+        },
     )
