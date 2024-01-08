7db8c565f5e5ab589b720ecff7d17105c5e738b6
Nicola Canepa <canne74@gmail.com>
2024-01-08 07:24:55 +0000

    [ompare_ka.py] Use hosts_if_not_me

--- src/tools/bin/compare_ka.py
+++ src/tools/bin/compare_ka.py
@@ -5,13 +5,14 @@ import attr
 import click
 
 from tools.bin.simple_service_map import HostChecker
+from tools.libs.net_utils import hosts_if_not_me
 
 
 @click.command()
 @click.argument("hostnames", nargs=-1) #, type=set)
 def main(hostnames: set):
     hc = HostChecker(hostnames)
-    for host in [h for h in hc.hosts if h.name != "phoenix"]:
+    for host in hosts_if_not_me(hc.hosts):
         click.echo(f"Checking {host.name}...")
         click.echo(check_output(["remote_diff.sh", "/etc/keepalived/keepalived.d/", f"{host.name}:/etc/keepalived/keepalived.d/"], universal_newlines=True, stderr=STDOUT))
 
