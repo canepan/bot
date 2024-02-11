6d300ab05d0464f4bd637d3c33a145722b6ea2cd
Nicola Canepa <canne74@gmail.com>
2024-02-11 13:21:51 +0000

    [service-map] Threads, show active on non primary host
    * remote checks run on threads by default
    * cache remote commands (in memory)
    * show "usurper" (master on a non primary host)
    * support reading status from daemon

--- src/tools/bin/simple_service_map.py
+++ src/tools/bin/simple_service_map.py
@@ -6,10 +6,11 @@ import re
 import sys
 import typing
 from collections import defaultdict
+from concurrent.futures import ThreadPoolExecutor
+from functools import lru_cache
 from getpass import getuser
 from glob import glob
 from subprocess import check_output, CalledProcessError, STDOUT
-from concurrent.futures import ThreadPoolExecutor
 
 import attr
 import click
@@ -17,14 +18,21 @@ import click
 from tools.libs.net_utils import ip_if_not_local
 
 KA_DIR = "/etc/keepalived/keepalived.d"
+KA_CONFIG_DIR = "/etc/keepalived"
+KA_CHECKS_DIR = os.path.join(KA_CONFIG_DIR, "bin")
 status_cache = defaultdict(dict)  # {host: {service: True/False}}
 
 
 def remote_command(host: str, cmd: list) -> str:
+    return _remote_command(host, " ".join(cmd))
+
+
+@lru_cache(maxsize=50)
+def _remote_command(host: str, cmd: str) -> str:
     if host:
-        run_cmd = ['ssh', host, " ".join(cmd)]
+        run_cmd = ['ssh', host, cmd]
     else:
-        run_cmd = ['bash', "-c", " ".join(cmd)]
+        run_cmd = ['bash', "-c", cmd]
     return check_output(run_cmd, universal_newlines=True, stderr=STDOUT)
 
 
@@ -34,6 +42,14 @@ def active(name: str) -> str:
 
 def is_active(name: str) -> bool:
     return name.startswith("+")
+
+
+def usurper(name: str) -> str:
+    return f"~{name}"
+
+
+def is_usurper(name: str) -> bool:
+    return name.startswith("~")
 
 
 def running(name: str) -> str:
@@ -52,8 +68,22 @@ def is_failed(name: str) -> bool:
     return name.startswith("*")
 
 
+def add_color(text: str) -> str:
+    if is_failed(text):
+        return click.style(text, "red", bold=True)
+    elif is_active(text):
+        return click.style(text, "green")
+    elif is_usurper(text):
+        return click.style(text, "yellow")
+    else:
+        return text
+
+
 @attr.s(hash=True)
 class Host(object):
+    """
+    Class to represent an host, wits its reachability status
+    """
     name: str = attr.ib()
 
     def __attrs_post_init__(self):
@@ -83,7 +113,54 @@ class Host(object):
             self.log.debug(f"Check {service} on {self}")
             if service.should_run_on(self):
                 if service.is_active_on(self):
+                    result.append(active(service.name) if self.name == service.hosts[0] else usurper(service.name))
+                elif service.is_running_on(self):
+                    result.append(running(service.name))
+                else:
+                    result.append(failed(service.name))
+            else:
+                self.log.debug(f"{service} should not run on {self}")
+        return result
+
+    def check_daemon_services(self, services: list) -> list:
+        if services is None:
+            return None
+        result = []
+        ka_data = {}
+        try:
+            ka_statuses = remote_command(
+                ip_if_not_local(self.name),
+                ["killall", "-USR1", "keepalived", ";", "cat /tmp/keepalived.data"]
+            )
+        except CalledProcessError as e:
+            ka_statuses = ""
+        keys = (
+            "Virtual Router ID",
+            "src_ip",
+            "transition",
+            "State",
+            "Interface",
+        )
+        click.echo(self)
+        for line in ka_statuses.splitlines():
+            if "Instance" in line and "=" in line:
+                service_name = line.split("=")[1].strip()
+                ka_data[service_name] = {}
+            for key in keys:
+                if key in line and "=" in line:
+                    value = line.split("=")[1].strip()
+                    if key != "State" or value in ("MASTER", "BACKUP", "FAIL"):
+                        ka_data[service_name][key] = value
+        click.echo(ka_data)
+
+        for service in services:
+            self.log.debug(f"Check {service} on {self}")
+            # if service.should_run_on(self):
+            if service.name in ka_data:
+                # if ka_data[service.is_active_on(self):
+                if ka_data[service.name].get("State") == "MASTER":
                     result.append(active(service.name))
+                # elif service.is_running_on(self):
                 elif service.is_running_on(self):
                     result.append(running(service.name))
                 else:
@@ -117,6 +194,18 @@ class ServiceStatus(object):
 
 
 class Service(object):
+    """
+    Class to represent a VRRP service, with a name and a configuration file
+    Fields:
+      * filename
+      * name
+      * hosts
+      * service_dict
+      * is_running_on(host): if the service is OK on the host
+      * is_active_on(host): if the host is the primary
+      * should_run_on(host): if the host has highest priority in config
+      * _status: dict of {'fqdn': ServiceStatus}
+    """
     def __init__(self, filename: str):
         if os.path.exists(filename):
             self._filename = filename
@@ -125,7 +214,6 @@ class Service(object):
             self._name = filename
             self._filename = os.path.join(KA_DIR, f"{filename}.conf")
         self._check_script = None
-        # self._config_name = None
         self._service_dict = None
         self._hosts = None
         self._status = defaultdict(ServiceStatus)
@@ -136,14 +224,6 @@ class Service(object):
         if self._check_script is None:
             self._service_dict = self.parse_config()
         return self._check_script
-
-    """
-    @property
-    def config_name(self):
-        if self._config_name is None:
-            self._service_dict = self.parse_config()
-        return self._config_name
-    """
 
     @property
     def filename(self):
@@ -174,8 +254,6 @@ class Service(object):
                 self.log.debug(f"Returned cache for {host.name}/+{self.name}")
             except KeyError:
                 try:
-                    # the following is not working
-                    # state = remote_command(ip_if_not_local(host.name), [f"awk -F ' - ' '{{print $2}}' '/tmp/{self.name}.state'"])
                     state = remote_command(ip_if_not_local(host.name), [f"cat '/tmp/{self.name}.state'"]).split("-")[1].strip()
                     self._status[host].active = state == "MASTER"
                 except CalledProcessError as e:
@@ -194,7 +272,7 @@ class Service(object):
                 self.log.debug(f"Returned cache for {host.name}/{self.name}")
             except KeyError:
                 try:
-                    remote_command(ip_if_not_local(host.name), [self.check_script])  # [f"/etc/keepalived/bin/check_{self.name}.sh"])
+                    remote_command(ip_if_not_local(host.name), [self.check_script])
                     self._status[host].available = True
                 except CalledProcessError as e:
                     self.log.debug(f"{e}\n{e.stdout}")
@@ -208,13 +286,14 @@ class Service(object):
     def parse_config(self) -> dict:
         with open(self.filename, 'r') as f:
             self._name = re.sub(r'\.conf$', '', os.path.basename(self.filename))
-            self._check_script = f"/etc/keepalived/bin/check_{self._name}.sh"
+            self._check_script = os.path.join(KA_CHECKS_DIR, f"check_{self._name}.sh")
             try:
                 first_line = f.readline()
                 for line in f:
-                    if line.strip().startswith("script "):
-                        self._check_sript = line.split()[1].strip('"')
-                    elif line.strip().startswith("vrrp_instance "):
+                    sline = line.strip()
+                    if sline.startswith("script "):
+                        self._check_script = line.split()[1].strip('"')
+                    elif sline.startswith("vrrp_instance "):
                         self._name = line.split()[1].strip('"')
                 return json.loads(first_line.lstrip('#').strip())
             except json.decoder.JSONDecodeError as e:
@@ -230,15 +309,6 @@ class Service(object):
         return f"{self.name} ({self.filename}): {', '.join(repr_hosts)}"
 
 
-def add_color(text: str) -> str:
-    if text.startswith("*"):
-        return click.style(text, "red", bold=True)
-    elif text.startswith("+"):
-        return click.style(text, "green")
-    else:
-        return text
-
-
 def show_services_by_host(active_services) -> typing.Iterator[str]:
     for host, services in active_services.items():
         if services is not None:
@@ -249,7 +319,9 @@ def show_services(active_services) -> typing.Iterator[str]:
     services_dict = defaultdict(list)
     for host, services in active_services.items():
         for service in services or []:
-            if is_active(service):
+            if is_usurper(service):
+                services_dict[service[1:]].append(usurper(host))
+            elif is_active(service):
                 services_dict[service[1:]].append(active(host))
             elif is_failed(service):
                 services_dict[service[1:]].append(failed(host))
@@ -262,7 +334,7 @@ def show_services(active_services) -> typing.Iterator[str]:
 def setup_logging(verbose: typing.Optional[bool]) -> logging.Logger:
     logging.basicConfig(stream=sys.stdout)
     log = logging.getLogger(__name__)
-    if user := f".{getuser()}" == ".root":
+    if (user := f".{getuser()}") == ".root":
         user = ""
     logfile = f"/tmp/{os.path.basename(__file__).split('.')[0]}{user}.log"
     fh = logging.FileHandler(logfile)
@@ -283,24 +355,24 @@ def setup_logging(verbose: typing.Optional[bool]) -> logging.Logger:
 @attr.s
 class HostChecker(object):
     hostnames: list = attr.ib()
+    query_daemon: bool = attr.ib(default=False)
 
     def __attrs_post_init__(self):
         self.log = logging.getLogger(__name__)
         self.service_list = {Service(fname) for fname in glob(os.path.join(KA_DIR, "*.conf"))}
         if self.hostnames:
-            self.hosts = [Host(hostname) for hostname in self.hostnames]
+            self.hosts = (Host(hostname) for hostname in self.hostnames)
         else:
-            self.hosts = []
-            for s in self.service_list:
-                for hostname in s.hosts:
-                    if Host(hostname) not in self.hosts:
-                        self.hosts.append(Host(hostname))
+            self.hosts = tuple(Host(hostname) for s in self.service_list for hostname in s.hosts)
             self.log.debug(f"Detected hosts: {', '.join(h.name for h in self.hosts)}")
 
     def check_host(self, host: Host):
         if host.is_reachable:
             self.log.debug(f"{host} is reachable: checking {self.service_list}")
-            return host.name, host.check_active_services(self.service_list)
+            if self.query_daemon and getuser() == "root":
+                return host.name, host.check_daemon_services(self.service_list)
+            else:
+                return host.name, host.check_active_services(self.service_list)
         else:
             return host.name, None
 
@@ -309,11 +381,13 @@ class HostChecker(object):
 @click.argument("hostnames", nargs=-1) #, type=set)
 @click.option("--by-service", "-s", default=False, is_flag=True)
 @click.option("--no-parallel", "-n", default=False, is_flag=True)
+@click.option("--query-daemon", "-D", default=False, is_flag=True, help="Root only")
 @click.option("--verbose/--quiet", "-v/-q", default=None)
-def main(hostnames: set, no_parallel: bool, by_service: bool, verbose: typing.Optional[bool]):
+def main(hostnames: set, no_parallel: bool, by_service: bool, query_daemon: bool, verbose: typing.Optional[bool]):
     log = setup_logging(verbose)
 
-    hc = HostChecker(hostnames)
+    log.debug(f"Starting with {hostnames}")
+    hc = HostChecker(hostnames, query_daemon)
     if no_parallel:
         active_services = defaultdict(list)
         for host in hc.hosts:
