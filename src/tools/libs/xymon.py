a6507c7797e869370e790b5ee7e34a6abcbbd3b0
Nicola Canepa <canne74@gmail.com>
2024-05-20 05:54:49 +0100

    [libs/xymon.py] Add tests, use f-strings, cleanup, fixes

--- src/tools/libs/xymon.py
+++ src/tools/libs/xymon.py
@@ -1,6 +1,8 @@
 import logging
 import os
 import socket
+import time
+from enum import Enum
 from subprocess import check_output
 
 APP_NAME = "XymonLib"
@@ -13,17 +15,12 @@ class XymonStatus(Enum):
 
 
 class Xymon(object):
-    GREEN = 0
-    YELLOW = 1
-    RED = 2
-    STATUS = ["green", "yellow", "red"]
-
     def __init__(self, cfg, check_name):
         os.environ["PATH"] += ":/usr/lib/xymon/client/bin"
         self.check_name = check_name
         self.host = (os.environ.get('CLIENTHOSTNAME', None) or socket.getfqdn()).replace(".", ",")
         self.debug = cfg.debug
-        self._log = logging.getLogger("{}.{}".format(APP_NAME, self.__class__.__name__))
+        self._log = logging.getLogger(f"{APP_NAME}.{self.__class__.__name__}")
 
     def send_status(self, status: XymonStatus, message: str):
         if self.debug:
@@ -31,23 +28,7 @@ class Xymon(object):
         else:
             debug = []
         for dest in os.environ.get("XYMONSERVERS", "192.168.0.68").split():
-            self._log.debug("Sending %s: %s\nfor %s to %s", Xymon.STATUS[status], message, self.host, dest)
-            self._log.info(
-                check_output(
-                    debug
-                    + [
-                        "xymon",
-                        dest,
-                        "status {}.{} {} {}\n{}".format(
-                            self.host,
-                            self.check_name,
-                            status.value,
-                            time.asctime(),
-                            message,
-                        ),
-                    ]
-                )
-            )
-
-
+            text = f"status {self.host}.{self.check_name} {status.value} {time.asctime()}\n{message}"
+            self._log.debug(f"Sending {status}: {message}\nfor {self.host} to {dest}")
+            self._log.info(check_output(debug + ["xymon", dest, text]))
 
---
+++ test/test_xymon.py
@@ -0,0 +1,48 @@
+from unittest import mock
+
+import pytest
+
+from tools.libs.xymon import Xymon, XymonStatus
+
+
+@pytest.fixture
+def mock_check_output(monkeypatch):
+    mock_obj = mock.Mock(name='check_output')
+    monkeypatch.setattr('tools.libs.xymon.check_output', mock_obj)
+    yield mock_obj
+    print(f'{mock_obj} {mock_obj.mock_calls}')
+
+
+@pytest.fixture
+def mock_socket(monkeypatch):
+    mock_obj = mock.Mock(name='socket')
+    mock_obj.getfqdn.return_value = "my.fqdn.tld"
+    monkeypatch.setattr('tools.libs.xymon.socket', mock_obj)
+    yield mock_obj
+    print(f'{mock_obj} {mock_obj.mock_calls}')
+
+
+@pytest.fixture
+def mock_time(monkeypatch):
+    mock_obj = mock.Mock(name='time')
+    monkeypatch.setattr('tools.libs.xymon.time', mock_obj)
+    yield mock_obj
+    print(f'{mock_obj} {mock_obj.mock_calls}')
+
+
+@pytest.mark.parametrize(
+    "status,message",
+    ((XymonStatus.GREEN, "All OK"), (XymonStatus.YELLOW, "Not great"), (XymonStatus.RED, "The end is nigh")),
+)
+def test_xymon(mock_check_output, mock_os, mock_socket, mock_time, status, message):
+    x = Xymon(mock.Mock(name="cfg", debug=False), "my_check_name")
+    x.send_status(status, message)
+    text = f"status my,fqdn,tld.my_check_name {status.value} {mock_time.asctime.return_value}\n{message}"
+    mock_check_output.assert_called_with(["xymon", "192.168.0.68", text])
+
+
+def test_xymon_debug(mock_check_output, mock_os, mock_socket, mock_time):
+    x = Xymon(mock.Mock(name="cfg", debug=True), "my_check_name")
+    x.send_status(XymonStatus.GREEN, "OK")
+    text = f"status my,fqdn,tld.my_check_name green {mock_time.asctime.return_value}\nOK"
+    mock_check_output.assert_called_with(["echo", "xymon", "192.168.0.68", text])
