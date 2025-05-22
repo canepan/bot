6a76a379b9773b1914eb2acc786059d3d26ecba9
Nicola Canepa <canne74@gmail.com>
2025-05-22 08:40:31 +0100

    [qsm] Show service names

--- src/tools/bin/quick_service_map.py
+++ src/tools/bin/quick_service_map.py
@@ -1,6 +1,10 @@
 #!/mnt/opt/nicola/tools/bin/python
+import json
 import os
+from glob import glob
 from subprocess import check_output
+
+from tools.bin.simple_service_map import Service
 
 
 def main():
@@ -8,17 +12,26 @@ def main():
     all_lines = check_output(cmd, universal_newlines=True).splitlines()
     cmd = ["ip", "-4", "-o", "ad", "sh", "dev", "eth0"]
     all_lines.extend(
-        f"{os.uname().nodename} {l}" for l in check_output(cmd, universal_newlines=True).splitlines()
+        f"{os.uname().nodename}: {l}" for l in check_output(cmd, universal_newlines=True).splitlines()
     )
+    ip_map = {}
     for line in all_lines:
-        hn, _, _, _, ip = line.split(max_split=5)
-        print(f"{hn} {ip}")
+        hn, _, _, _, ip, _ = line.split(maxsplit=5)
+        ip_map[ip] = {"hostname": hn.rstrip(":")}
+    configs = {}
+    for conf_file in glob("/etc/keepalived/keepalived.d/*.conf"):
+        service = Service(conf_file)
+        configs[conf_file] = Service(conf_file)
+    for ip, data in ip_map.items():
+        for conf_file, config_content in configs.items():
+            if ip == config_content.service_dict["ip"]:
+                data["name"] = configs[conf_file].name
+                data["config"] = configs[conf_file].service_dict
+                print(f"{data['name']} ({ip}): {data['hostname']}")
 
 
 if __name__ == "__main__":
     main()
-    sys.exit(0)
-
 
 """
 #!/bin/bash
